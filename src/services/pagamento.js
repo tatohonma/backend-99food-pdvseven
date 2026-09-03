bdadad8ce170f8ea90bb1db88a6f1b619412dc24
maikon alexandre <maikonalexandre574@gmail.com>
2026-09-03 17:25:52 -0300

    fix: streamline payment processing logic in adicionarPagamento function

--- src/services/pagamento.js
+++ src/services/pagamento.js
@@ -3,39 +3,41 @@ import { criarPedidoPagamento } from "../repositories/pedido_pagamento.js";
 import { calcularTotais } from "./calculo_pedido.js";
 
 export const adicionarPagamento = async ({ idPedido, pedido }) => {
-  const mapTipoPagamento = {
-    150: configuracoes.tipoPagamento.credito, // 150 é cartão crédito ou débito
-    153: configuracoes.tipoPagamento.dinheiro,
-    212: configuracoes.tipoPagamento.pix,
-  };
+	const mapTipoPagamento = {
+		150: configuracoes.tipoPagamento.credito, // 150 é cartão crédito ou débito
+		153: configuracoes.tipoPagamento.dinheiro,
+		212: configuracoes.tipoPagamento.pix,
+	};
 
-  const { valorTotal } = calcularTotais(pedido);
-  const valorDoPagamento = valorTotal;
+	const { valorTotal } = calcularTotais(pedido);
+	const valorDoPagamento = valorTotal;
 
-  const pagamentoRealizadoNa99Food =
-    pedido.delivery_type === 1 || pedido.pay_method === 1;
+	const pagamentoRealizadoNa99Food =
+		pedido.delivery_type === 1 || pedido.pay_method === 1;
 
-  const idTipoPagamento = pagamentoRealizadoNa99Food
-    ? configuracoes.tipoPagamento["99Food"]?.IDTipoPagamento
-    : mapTipoPagamento[pedido.pay_channel]?.IDTipoPagamento;
+	const idTipoPagamento = pagamentoRealizadoNa99Food
+		? configuracoes.tipoPagamento["99Food"]?.IDTipoPagamento
+		: mapTipoPagamento[pedido.pay_channel]?.IDTipoPagamento;
 
-  const tipoPagamentoOutros = configuracoes.tipoPagamento.outros;
+	const tipoPagamentoOutros = configuracoes.tipoPagamento.outros;
 
-  if (!idTipoPagamento && !tipoPagamentoOutros) {
-    throw new Error(
-      "Nenhum tipo de pagamento configurado no PDV7 para o pedido 99Food",
-    );
-  }
+	if (!idTipoPagamento && !tipoPagamentoOutros) {
+		throw new Error(
+			"Nenhum tipo de pagamento configurado no PDV7 para o pedido 99Food",
+		);
+	}
 
-  await criarPedidoPagamento({
-    idPedido,
-    valorDoPagamento: valorDoPagamento / 100,
-    idGateway: null,
-    IDTipoPagamento: idTipoPagamento ?? tipoPagamentoOutros?.IDTipoPagamento,
-    IDUsuario: configuracoes.usuario.IDUsuario,
-  });
+	await criarPedidoPagamento({
+		idPedido,
+		valorDoPagamento: pagamentoRealizadoNa99Food
+			? valorDoPagamento / 100
+			: (pedido?.change_for ?? valorDoPagamento) / 100,
+		idGateway: null,
+		IDTipoPagamento: idTipoPagamento ?? tipoPagamentoOutros?.IDTipoPagamento,
+		IDUsuario: configuracoes.usuario.IDUsuario,
+	});
 
-  return {
-    value: parseFloat(valorDoPagamento),
-  };
+	return {
+		value: parseFloat(valorDoPagamento),
+	};
 };
